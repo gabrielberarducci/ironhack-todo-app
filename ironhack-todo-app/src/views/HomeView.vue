1231d30247a6413817e9cdf41f8a2bcaf88cc6bb
Gabriel <gabrielberarducci@gmail.com>
2023-07-08 11:26:54 +0200

    Clean installation

--- ironhack-todo-app/src/views/HomeView.vue
+++ ironhack-todo-app/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>This is the Home page</h1>
+  </div>
 </template>
